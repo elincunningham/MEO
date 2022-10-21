1f836a9dd622c0ee9fd434139d98c7605b3c8a96
Elin Cunningham <62517656+elincunningham@users.noreply.github.com>
2022-10-21 09:49:41 -0230

    Added solution to sum exercise

---
+++ Solution to sum excercise.py
@@ -0,0 +1,7 @@
+number1 = int(input( 'Enter you first number:' ))
+number2 = int(input( 'Enter your second number:' ))
+number3 = int(input( 'Enter your third number:' ))
+
+sum = number1 + number2 + number3
+
+print( 'The sum of your number is' , sum )
